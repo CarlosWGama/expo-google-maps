ecc0ceec8fd25e427976071dbd3f8bd91cded3ce
Carlos W. Gama <carloswgama@gmail.com>
2022-02-10 14:10:45 -0300

    Inicializado

--- App.tsx
+++ App.tsx
@@ -1,10 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+
+
+  const [liberado, setLiberado] = useState(false);
+  const [location, setLocation] = useState<null|{latitude:number, longitude:number}>(null);
+  //Rodar o comando: expo install react-location (https://docs.expo.dev/versions/v44.0.0/sdk/location/)
+  //Rodar o comando: expo install react-native-maps (https://docs.expo.dev/versions/v44.0.0/sdk/map-view/)
+
+  
+  const abrirMapa =  async () => {
+    //Solicita a permissão
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('É preciso dar permissão para continuar')
+    } else {
+
+      //Recupera a localização
+      let location = await Location.getCurrentPositionAsync({});
+      const {latitude, longitude} = location.coords;
+      setLocation({latitude, longitude});
+
+
+      
+      setLiberado(true);
+    }
+
+  }
+  
+  //Essa função só e executada uma unica vez quando a tela carrega
+  useEffect(() => {
+    abrirMapa();
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {!liberado && (
+        <>
+          <Text>Permissões não liberadas</Text>
+          <Button title='Liberar' onPress={abrirMapa}/>
+        </>
+      )}
+
+      {/* LATIDUDE E LONGITUDE */}
+      {location != null && (
+        <>
+
+          <Text>Latitude: {location.latitude}</Text>
+          <Text>Longitude: {location.longitude}</Text>
+
+          {/* MAPA */}
+          {liberado && (
+              <MapView 
+                style={{width: '100%', height: 400}}
+                initialRegion={{...location, latitudeDelta: 0.01, longitudeDelta: 0.01}}  
+              >
+                {/* MARKER É o ponteiro */}
+                <Marker
+                  coordinate={location}
+                  title="Local definido"
+                />
+              </MapView>
+          )}
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
